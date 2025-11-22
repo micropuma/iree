83475ad5fd380d02267aad22b83ac3a8f37f4954
micropuma <micropuma@163.com>
2025-11-22 22:33:48 +0800

    [test] trace vm invocation process

--- tutorial/Stable-HLO/model/LSTM/run.sh
+++ tutorial/Stable-HLO/model/LSTM/run.sh
@@ -18,6 +18,8 @@ iree-compile --iree-hal-target-backends=cuda \
              LSTM.mlir -o LSTM.vmfb
 
 iree-run-module --device=cuda \
+                --trace_execution \
                 --module=LSTM.vmfb \
                 --input=1x5xf32=[0,1,0,3,4] \
-                --input=1x5x2x2xf32=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+                --input=1x5x2x2xf32=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] \
+                2>&1 | tee run-module.log
